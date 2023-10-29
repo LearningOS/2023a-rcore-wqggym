c0e6e5e9aa8c0648e7f27828517d0fc108207f95
hp王啟刚 <499811507@qq.com>
2023-10-29 18:41:51 +0800

    edit

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,14 +1,14 @@
-//! Implementation of syscalls
+//! Implementation of sys calls
 //!
 //! The single entry point to all system calls, [`syscall()`], is called
-//! whenever userspace wishes to perform a system call using the `ecall`
+//! whenever userspace wishes to perform a system call using the `recall`
 //! instruction. In this case, the processor raises an 'Environment call from
 //! U-mode' exception, which is handled as one of the cases in
 //! [`crate::trap::trap_handler`].
 //!
 //! For clarity, each single syscall is implemented as its own function, named
 //! `sys_` then the name of the syscall. You can find functions like this in
-//! submodules, and you should also implement syscalls this way.
+//! submodules, and you should also implement sys calls this way.
 
 /// write syscall
 const SYSCALL_WRITE: usize = 64;
@@ -16,9 +16,9 @@ const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 /// yield syscall
 const SYSCALL_YIELD: usize = 124;
-/// gettime syscall
+/// getting syscall
 const SYSCALL_GET_TIME: usize = 169;
-/// taskinfo syscall
+/// task info syscall
 const SYSCALL_TASK_INFO: usize = 410;
 
 mod fs;
