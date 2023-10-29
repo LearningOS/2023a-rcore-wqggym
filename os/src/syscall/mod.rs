e8ea2f2c1646fe0e1c1bbe4eaee60169eaee97b6
hp王啟刚 <499811507@qq.com>
2023-10-29 17:40:46 +0800

    edit

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,20 +1,25 @@
-//! Implementation of sys calls
+//! Implementation of syscalls
 //!
 //! The single entry point to all system calls, [`syscall()`], is called
-//! whenever userspace wishes to perform a system call using the `recall`
+//! whenever userspace wishes to perform a system call using the `ecall`
 //! instruction. In this case, the processor raises an 'Environment call from
 //! U-mode' exception, which is handled as one of the cases in
 //! [`crate::trap::trap_handler`].
 //!
 //! For clarity, each single syscall is implemented as its own function, named
 //! `sys_` then the name of the syscall. You can find functions like this in
-//! submodules, and you should also implement sys calls this way.
+//! submodules, and you should also implement syscalls this way.
 
-pub const SYSCALL_WRITE: usize = 64;
-pub const SYSCALL_EXIT: usize = 93;
-pub const SYSCALL_YIELD: usize = 124;
-pub const SYSCALL_GET_TIME: usize = 169;
-pub const SYSCALL_TASK_INFO: usize = 410;
+/// write syscall
+const SYSCALL_WRITE: usize = 64;
+/// exit syscall
+const SYSCALL_EXIT: usize = 93;
+/// yield syscall
+const SYSCALL_YIELD: usize = 124;
+/// gettime syscall
+const SYSCALL_GET_TIME: usize = 169;
+/// taskinfo syscall
+const SYSCALL_TASK_INFO: usize = 410;
 
 mod fs;
 mod process;
@@ -26,18 +31,14 @@ use crate::task::add_syscall_time;
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+    // record syscall
     add_syscall_time(syscall_id);
-
     match syscall_id {
-        SYSCALL_WRITE => unsafe { sys_write(args[0], args[1] as *const u8, args[2]) },
-        SYSCALL_EXIT => unsafe { sys_exit(args[0] as i32) },
-        SYSCALL_YIELD => unsafe { sys_yield() },
-        SYSCALL_GET_TIME => unsafe { sys_get_time(args[0] as *mut TimeVal, args[1]) },
-        SYSCALL_TASK_INFO => unsafe {
-            sys_task_info(args[0] as *mut TaskInfo)
-                .unwrap()
-        },
+        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
+        SYSCALL_EXIT => sys_exit(args[0] as i32),
+        SYSCALL_YIELD => sys_yield(),
+        SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
+        SYSCALL_TASK_INFO => sys_task_info(args[0] as *mut TaskInfo),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
-
